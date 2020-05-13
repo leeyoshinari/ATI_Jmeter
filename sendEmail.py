bee21b5bf257a261d8a6b25a74215f0b53eb1f08
leeyoshinari <lee_jc@outlook.com>
2020-05-13 22:43:02 +0800

    modify email

--- sendEmail.py
+++ sendEmail.py
@@ -21,12 +21,14 @@ def sendMsg(html, receiver_email, is_path=True):
         try:
             receive_name = re.findall('email_(.*?).txt', receiver_email)[0]     # 提取收件人姓名
             with open(receiver_email, 'r', encoding='utf-8') as f:
-                send_to = f.read()
+                sends = f.readlines()
+            subject = sends[0]
+            send_to = sends[1]
             logger.info('开始发送邮件，收件人{}'.format(send_to))
             message = MIMEMultipart()
             message['From'] = Header(cfg.getConfig('sender_name'))  # 发件人名字
             message['To'] = Header(receive_name)  # 收件人名字
-            message['Subject'] = Header(cfg.getConfig('subject'), 'utf-8')  # 邮件主题
+            message['Subject'] = Header(subject, 'utf-8')  # 邮件主题
 
             if is_path:
                 with open(html, 'r', encoding='utf-8') as f:
@@ -38,12 +40,12 @@ def sendMsg(html, receiver_email, is_path=True):
             message.attach(email_text)  # 添加邮件正文
 
             try:
-                server = smtplib.SMTP_SSL(cfg.getConfig('smtp'))
-                server.connect(cfg.getConfig('smtp'))
+                server = smtplib.SMTP_SSL(cfg.getConfig('smtp'), 465)
+                # server.connect(cfg.getConfig('smtp'))
             except Exception as err:
                 logger.error(err)
-                server = smtplib.SMTP()
-                server.connect(cfg.getConfig('smtp'))
+                server = smtplib.SMTP(cfg.getConfig('smtp'), 25)
+                # server.connect(cfg.getConfig('smtp'))
 
             server.login(cfg.getConfig('sender_email'), '123456')  # 登陆邮箱
             server.sendmail(cfg.getConfig('sender_email'), send_to.split(','), message.as_string())  # 发送邮件
